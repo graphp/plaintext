a08fe3151260105ba50a7db35b61c5b65690cafd
Christian Lück <christian@lueck.tv>
2012-04-17 12:57:25 +0200

    Update project directory structure

---
+++ src/LoaderEdgeListWeighted.php
@@ -0,0 +1,39 @@
+<?php
+
+class LoaderEdgeListWeighted extends LoaderFile{
+	
+	private $debugMode = false;
+	
+	private function writeDebugMessage($messageString){
+		if($this->debugMode){
+			echo $messageString;
+		}
+	}
+	
+	public function __construct(){
+	}
+	
+	public function getGraph(){
+		
+		$graph = new Graph();
+		
+		$file = file($this->fileName, FILE_IGNORE_NEW_LINES);
+		$vertexCount = $file[0];
+		$edgeCounter = 0;
+		
+		$graph->createVertices($vertexCount);
+		
+		unset($file[0]);
+		foreach ($file as $zeile) {
+			$edgeConnections = explode("\t", $zeile);
+			
+			$from = $graph->getVertex($edgeConnections[0]);
+			$to = $graph->getVertex($edgeConnections[1]);
+			
+			$from->createEdge($to,$edgeConnections[2]);								//TODO directed
+		}
+		
+		return $graph;
+		
+	}	
+}
